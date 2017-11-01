bdf02d674449841eb58d9ec88be16ab9f6c88294
carlos <carlos@cdis.com.br>
2017-11-01 10:55:41 -0200

    Atualização JS

---
+++ js/config.js
@@ -0,0 +1,11 @@
+
+function setConfig() {
+
+	var texts = {
+		"title":"App de Controle JS"
+	};
+	document.title = texts.title;
+	document.getElementById('navTitle').innerHTML = texts.title;
+}
+
+setConfig();
